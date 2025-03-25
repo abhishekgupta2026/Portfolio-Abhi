55f7e6594e7c56c3e3339670e0edb6d8a1d60ad8
abhishekgupta2026 <abhishekkumargupta2026@gmail.com>
2025-03-25 16:46:26 +0530

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,15 @@
 const AboutPage = () => {
   return (
     <>
-      <h3>Enthusiastic and dedicated B.Tech student in Computer Science with a specialization in Gaming Technology, possessing a strong foundation in software development, data structures and algorithms. Passionate about problem-solving and optimizing code efficiency, I have hands-on experience in full-stack web development using JavaScript, React, Node.js, and databases. I enjoy building seamless, user-friendly applications, and my background in gaming technology enables me to explore interactive experiences, game mechanics, and immersive UI/UX design.
+<h3> I am a B.Tech student at VIT University, specializing in Computer Science, with a strong passion for MERN Stack Development and a solid foundation in Data Structures and Algorithms and Object-Oriented Programming. Proficient in multiple programming languages such as C++, Java, Python, and JavaScript, I have hands-on experience with front-end technologies like React.js, Next.js, Redux Toolkit, Tailwind CSS, Bootstrap, and UI/UX design principles. On the back-end, I specialize in Node.js, Express.js, and MongoDB, along with Firebase for authentication and real-time databases. My expertise extends to working with libraries like numpy and pandas for data processing. I actively use GitHub for version control and collaborate on projects efficiently. With a keen interest in building scalable and interactive web applications, I continuously explore new tools and frameworks to enhance my development skills.
+
+
+
+
+
+
+
+
 
 
 
