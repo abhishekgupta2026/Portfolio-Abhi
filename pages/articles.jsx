adb30d9c44f4b611f28f51fb21677a3e0b6d626f
abhishekgupta2026 <abhishekkumargupta2026@gmail.com>
2025-03-25 16:49:07 +0530

    Update articles.jsx

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -1,48 +1,70 @@
 import ArticleCard from '../components/ArticleCard';
 import styles from '../styles/ArticlesPage.module.css';
 
-const ArticlesPage = ({ articles }) => {
-  if (!Array.isArray(articles)) {
-    return <p>No articles available.</p>;
-  }
+const skills = {
+  "Languages & Libraries": [
+    "C++", "Java", "HTML5", "Python", "numpy", "pandas", "CSS3", "JavaScript",
+    "Express.js", "jQuery", "TypeScript", "Data Structures & Algorithms", "OOP"
+  ],
+  "Tools & Software": [
+    "Jupyter Notebook", "MySQL", "React.js", "Next.js", "Redux Toolkit", "Tailwind CSS",
+    "Bootstrap", "MongoDB", "GitHub", "Node.js", "Express.js", "Firebase", "UI/UX", "DBMS"
+  ]
+};
 
+const ArticlesPage = ({ articles }) => {
   return (
     <>
-      <h3>
-        Recent Posts from{' '}
-        <a
-          href="https://dev.to/abhishek_kumargupta2"
-          target="_blank"
-          rel="noopener"
-          className={styles.underline}
-        >
-          DEV.TO
-        </a>
+      <div className={styles.pageContainer}>
+        {/* Articles Column */}
+        <div className={styles.articlesColumn}>
+          <h3>
+            Recent Posts from{' '}
+            <a
+              href="https://dev.to/abhishek_kumargupta2"
+              target="_blank"
+              rel="noopener"
+              className={styles.underline}
+            >
+              DEV.TO
+            </a>
+          </h3>
+          <h3>
+            CODING PROFILE{' '}
+            <a
+              href="https://leetcode.com/u/Abhishek78678/"
+              target="_blank"
+              rel="noopener"
+              className={styles.underline}
+            >
+              LEETCODE.PRO
+            </a>
+          </h3>
+          <div className={styles.container}>
+            {articles.length > 0 &&
+              articles.map((article) => (
+                <ArticleCard key={article.id} article={article} />
+              ))}
+          </div>
+        </div>
 
-        
-      </h3>
-      
-      <h3>
-        CODING PROFILE{' '}
-        <a
-          href="https://leetcode.com/u/Abhishek78678/"
-          target="_blank"
-          rel="noopener"
-          className={styles.underline}
-        >
-          LEETCODE.PRO
-        </a>
-
-        
-      </h3>
-      <div className={styles.container}>
-        {articles.length > 0 ? (
-          articles.map((article) => (
-            <ArticleCard key={article.id} article={article} />
-          ))
-        ) : (
-          <p></p>
-        )}
+        {/* Skills Column */}
+        <div className={styles.skillsColumn}>
+          <h3>Skills</h3>
+          {Object.entries(skills).map(([category, items]) => (
+            <div key={category} className={styles.skillCategory}>
+              <h4 className={styles.skillHeading}>{category}</h4>
+              <hr className={styles.skillDivider} />
+              <div className={styles.skillSpacing}></div>
+              <ul>
+                {items.map((skill, index) => (
+                  <li key={index}>{skill}</li>
+                ))}
+              </ul>
+              <br />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
@@ -64,8 +86,6 @@ export async function getStaticProps() {
     }
 
     const data = await res.json();
-
-    // Ensure data is an array
     const articles = Array.isArray(data) ? data : [];
 
     return {
@@ -81,3 +101,4 @@ export async function getStaticProps() {
 }
 
 export default ArticlesPage;
+
